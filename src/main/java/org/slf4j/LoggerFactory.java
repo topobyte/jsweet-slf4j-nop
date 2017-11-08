869614f4dca9a9b3a8e7e1cc28674ce61ae22735
Sebastian Kürten <sebastian@topobyte.de>
2017-11-08 11:06:53 +0100

    Fix typo: slfj4 -> slf4j

--- src/main/java/org/slf4j/LoggerFactory.java
+++ src/main/java/org/slf4j/LoggerFactory.java
@@ -1,19 +1,19 @@
 // Copyright 2017 Sebastian Kuerten
 //
-// This file is part of jsweet-slfj4-nop.
+// This file is part of jsweet-slf4j-nop.
 //
-// jsweet-slfj4-nop is free software: you can redistribute it and/or modify
+// jsweet-slf4j-nop is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// jsweet-slfj4-nop is distributed in the hope that it will be useful,
+// jsweet-slf4j-nop is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with jsweet-slfj4-nop. If not, see <http://www.gnu.org/licenses/>.
+// along with jsweet-slf4j-nop. If not, see <http://www.gnu.org/licenses/>.
 
 package org.slf4j;
 
